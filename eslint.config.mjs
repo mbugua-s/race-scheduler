ed0a401c58d7ce1292c79e7defd1b5fe81cf3162
mbugua-s <smbuguaaa@gmail.com>
2025-10-29 22:51:08 +0300

    chore: Set up env variable loading and git hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,38 @@
-// @ts-check
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import js from '@eslint/js';
 
-export default defineConfig({
-	ignores: ['eslint.config.mjs'],
-
-	// Base ESLint, Prettier, and TypeScript recommended configs
-	extends: [
-		'eslint:recommended',
-		...tseslint.configs.recommendedTypeChecked,
-		'prettier',
-	],
-
-	languageOptions: {
-		globals: {
-			...globals.node,
-			...globals.jest,
-		},
-		sourceType: 'commonjs',
-		parserOptions: {
-			projectService: true,
-			tsconfigRootDir: import.meta.dirname,
-		},
+export default defineConfig([
+	{
+		ignores: [
+			'eslint.config.mjs',
+			'**/.husky/**',
+			'**/generated/**',
+			'**/node_modules/**',
+			'**/dist/**',
+			'commitlint.config.js',
+		],
 	},
 
-	rules: {
-		'@typescript-eslint/no-floating-promises': 'warn',
-		'@typescript-eslint/no-unsafe-argument': 'warn',
-		'prettier/prettier': ['error', { endOfLine: 'auto' }],
+	js.configs.recommended,
+	...tseslint.configs.recommendedTypeChecked,
+
+	{
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.jest,
+			},
+			sourceType: 'commonjs',
+			parserOptions: {
+				project: './tsconfig.json',
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+		rules: {
+			'@typescript-eslint/no-floating-promises': 'warn',
+			'@typescript-eslint/no-unsafe-argument': 'warn',
+		},
 	},
-});
+]);
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,31 +4,24 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { EnvSchema } from './config/env.schema';
 import { PrismaService } from './prisma/prisma.service';
+import { config as dotenvConfig } from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
 // import { PrismaModule } from './prisma/prisma.module';
 
-// 1. Determine the environment
 const env = process.env.NODE_ENV || 'production';
-
-// 2. Build the array containing ONLY the specific environment file
 const envFilePaths = [
 	`.env.${env}`, // e.g., .env.development or .env.production
 ];
 
-// Optional: Log the file path for debugging
 console.log(`Loading config from: ${envFilePaths[0]}`);
+dotenvExpand.expand(dotenvConfig({ path: envFilePaths[0] }));
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
-
-			// Pass the array with only the specific file path
 			envFilePath: envFilePaths,
 
-			// Optional: Set 'ignoreEnvFile' to 'false' (default) to ensure it uses the file.
-			// Setting 'ignoreEnvFile: true' would ignore the file and only use process.env.
-
-			// Important: Your Zod validation (next step) handles "always set" check
 			validate: (config) => {
 				const parsed = EnvSchema.safeParse(config);
 				if (!parsed.success) {
